25593623d1a1dbef899f092c499db59c3a09b1be
Diogo <diogowukectt@gmail.com>
2024-06-06 20:01:50 -0300

    final

--- app/src/main/java/com/example/projetointegrador/MainActivity.java
+++ app/src/main/java/com/example/projetointegrador/MainActivity.java
@@ -1,28 +1,102 @@
 package com.example.projetointegrador;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.projetointegrador.Adapter.ToDoAdapter;
+import com.example.projetointegrador.Model.ToDoModel;
+import com.example.projetointegrador.Utils.DatabaseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DialogCloseListener {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+    private FloatingActionButton fab;
+
+
+    private List<ToDoModel> taskList;
+    private DatabaseHandler db;
+
+    Dialog mDialog;
+    Button helpButton;
+
+
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button=findViewById(R.id.buttonModelo1);
-        button.setOnClickListener(new View.OnClickListener() {
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter (db, MainActivity.this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        fab = findViewById(R.id.fab);
+
+        ItemTouchHelper itemTouchHelper = new
+                ItemTouchHelper(new RecyclerItemTouchHelper((tasksAdapter)));
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView);
+
+        List<ToDoModel> taskList = db.getAllTasks();
+        tasksAdapter.setTasks(taskList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View view) {
-
-                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(),AddNewTask.TAG);
             }
-
         });
+
+        ImageButton helpButton = findViewById(R.id.helpButton);
+
+        mDialog = new Dialog (this);
+
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View v) {
+                mDialog.setContentView(R.layout.popup);
+                mDialog.getWindow().setBackgroundDrawable((new ColorDrawable(android.R.color.transparent)));
+                mDialog.show();
+            }
+        });
+
+
+
+
+
     }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
+    }
+
 }
